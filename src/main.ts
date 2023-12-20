3518a4bbadc13d08000267883c70efc46f4ebbbb
anierbeck <achim.nierbeck@codecentric.de>
2023-12-20 10:26:02 +0100

    Initial commit of first working version

---
+++ src/main.ts
@@ -0,0 +1,177 @@
+/*
+ * Created with @iobroker/create-adapter v2.5.0
+ */
+"use strict";
+
+// The adapter-core module gives you access to the core ioBroker functions
+// you need to create an adapter
+import * as utils from "@iobroker/adapter-core";
+import axios from "axios";
+
+const adapterName = require("./../package.json").name.split(".").pop();
+const instanceObjects = require("./../io-package.json").instanceObjects;
+
+interface State {
+	from: string;
+	to: string;
+	state: number;
+}
+
+class Stromgedacht extends utils.Adapter {
+	public constructor(options: Partial<utils.AdapterOptions> = {}) {
+		super({
+			...options,
+			name: adapterName,
+		});
+
+		this.setState("info.connection", false, true);
+
+		this.on("ready", this.onReady.bind(this));
+		this.on("unload", this.onUnload.bind(this));
+	}
+
+	/**
+	 * Is called when databases are connected and adapter received configuration.
+	 */
+	async onReady(): Promise<void> {
+		for (const obj of instanceObjects) {
+			await this.setObjectNotExistsAsync(obj._id, obj);
+		}
+
+		//schedule it to run every 15 minutes
+		this.log.info(`config zipcode: ${this.config.zipcode}`);
+
+		this.setState("info.connection", false, true);
+
+		if (this.config.zipcode === undefined || this.config.zipcode === "") {
+			this.log.error("No zipcode configured");
+			return;
+		}
+
+		this.getStatesOf("forecast", "", async (err, states: any) => {
+			if (err) {
+				this.log.error(`Could not get states of forecast: ${err.message}`);
+				return;
+			}
+			//removing all states
+			/*
+			for (const state of states) {
+				this.log.info(`Deleting state ${state._id}`);
+				await this.delObjectAsync(state._id);
+			}
+			*/
+		});
+
+		this.getStatesOf("states", "", async (err, states: any) => {
+			if (err) {
+				this.log.error(`Could not get states of states: ${err.message}`);
+				return;
+			}
+			for (const state of states) {
+				this.log.info(`Deleting state ${state._id}`);
+				await this.delObjectAsync(state._id);
+			}
+		});
+
+		//this.requestForecast();
+		this.requestStates();
+	}
+
+	/**
+	 * Is called when adapter shuts down - callback has to be called under any circumstances!
+	 */
+	private onUnload(callback: () => void): void {
+		try {
+			// Here you must clear all timeouts or intervals that may still be active
+			// clearTimeout(timeout1);
+			// clearTimeout(timeout2);
+			// ...
+			// clearInterval(interval1);
+
+			callback();
+		} catch (e) {
+			callback();
+		}
+	}
+
+	requestStates(): void {
+		const zipcode = this.config.zipcode;
+		const url = "https://api.stromgedacht.de/v1/statesRelative?zip=";
+		const fullUrl = url + zipcode + "&hoursInFuture=24";
+		this.log.info(`Requesting states for ${zipcode} from ${fullUrl}`);
+
+		const queryParams = {
+			zip: zipcode,
+			hoursInFuture: 24,
+		};
+
+		axios({
+			method: "get",
+			baseURL: "https://api.stromgedacht.de/v1/statesRelative",
+			params: queryParams,
+			timeout: 10000,
+			responseType: "json",
+			validateStatus: (status) => status === 200,
+		})
+			.then(async (response) => {
+				this.log.info(`Received states for ${zipcode}: ${JSON.stringify(response.data)}`);
+				this.setState("forecast.states.json", JSON.stringify(response.data), true);
+				this.setState("forecast.states.hoursInFuture", this.config.hoursInFuture, true);
+				this.parseState(response.data);
+			})
+			.catch((error) => {
+				if (error.response) {
+					this.log.error(`Error: ${error.response.status}`);
+				} else if (error.request) {
+					this.log.error(`Error: no data received for Current Weather data`);
+				} else {
+					this.log.error(`Error: ${error.message}`);
+				}
+				console.log(error.config);
+			});
+		this.setState("info.connection", true, true);
+	}
+
+	parseState(json: any): void {
+		this.log.info(`Parsing state ${JSON.stringify(json)}`);
+		let states: State[] = [];
+		//Object.assign(states, json.states);
+		states = json.states;
+		this.log.debug(`States: ${JSON.stringify(states)}`);
+		let stateId = "";
+		states.forEach((state: any) => {
+			switch (state.state) {
+				case -1: //supergruen
+					stateId = "forecast.states.supergruen";
+					this.log.info(`state ${stateId}`);
+					break;
+				case 1: //gruen
+					stateId = "forecast.states.gruen";
+					this.log.info(`state ${stateId}`);
+					break;
+				case 2: //gelb
+					stateId = "forecast.states.gelb";
+					this.log.info(`state ${stateId}`);
+					break;
+				case 3: //rot
+					stateId = "forecast.states.rot";
+					this.log.info(`state ${stateId}`);
+					break;
+				default:
+					break;
+			}
+			this.log.info(`Setting state ${stateId}.begin to ${state.from}`);
+			this.setState(stateId + ".begin", state.from, true);
+			this.log.info(`Setting state ${stateId}.end to ${state.to}`);
+			this.setState(stateId + ".end", state.to, true);
+		});
+	}
+}
+
+if (require.main !== module) {
+	// Export the constructor in compact mode
+	module.exports = (options: Partial<utils.AdapterOptions> | undefined) => new Stromgedacht(options);
+} else {
+	// otherwise start the instance directly
+	(() => new Stromgedacht())();
+}
---
+++ test/integration.js
@@ -0,0 +1,76 @@
+const path = require("path");
+const { tests, utils } = require("@iobroker/testing");
+const { util } = require("chai");
+const assert = require("assert");
+
+const adapterName = require("./../package.json").name.split(".").pop();
+
+// Run integration tests - See https://github.com/ioBroker/testing for a detailed explanation and further options
+tests.integration(path.join(__dirname, ".."), {
+	//            ~~~~~~~~~~~~~~~~~~~~~~~~~
+	// This should be the adapter's root directory
+
+	// If the adapter may call process.exit during startup, define here which exit codes are allowed.
+	// By default, termination during startup is not allowed.
+	allowedExitCodes: [11],
+
+	// To test against a different version of JS-Controller, you can change the version or dist-tag here.
+	// Make sure to remove this setting when you're done testing.
+	controllerVersion: "latest", // or a specific version like "4.0.1"
+
+	// Define your own tests inside defineAdditionalTests
+	defineAdditionalTests({ suite }) {
+		// All tests (it, describe) must be grouped in one or more suites. Each suite sets up a fresh environment for the adapter tests.
+		// At the beginning of each suite, the databases will be reset and the adapter will be started.
+		// The adapter will run until the end of each suite.
+
+		// Create mocks and asserts
+		const { adapter, database } = utils.unit.createMocks();
+		const { assertObjectExists } = utils.unit.createAsserts(database, adapter);
+
+		// Since the tests are heavily instrumented, each suite gives access to a so called "harness" to control the tests.
+		suite("Test retrieveJson()", (getHarness) => {
+			// For convenience, get the current suite's harness before all tests
+			let harness;
+			before(async () => {
+				harness = getHarness();
+
+				const obj = {
+					"native": {
+						"zipcode": "76135",
+						"hoursInFuture": "24",
+					},
+				};
+				await harness.changeAdapterConfig(adapterName, obj);
+			});
+
+			afterEach(() => {
+				// The mocks keep track of all method invocations - reset them after each single test
+				adapter.resetMockHistory();
+				// We want to start each test with a fresh database
+				database.clear();
+			});
+
+			it("Should work", () => {
+				return new Promise(async (resolve) => {
+					// Start the adapter and wait until it has started
+
+					//log.debug("check if zipcode is configured");
+
+					//harness.databases.adapter.config.zipcode.should.equal("72135");
+					log.debug("Start adapter");
+					await harness.startAdapterAndWait();
+
+					// Perform the actual test:
+					harness.states.getState("stromgedacht.0.forecast.states.json").val.should.not.equal(null);
+					/*
+					harness.sendTo("adapter.0", "test", "message", (resp) => {
+						console.dir(resp);
+						resolve();
+					});
+					*/
+				});
+			}).timeout(2000);
+		});
+	},
+});
